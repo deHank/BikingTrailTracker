cef0ae50b7d1dab31b6ece330bd138703b2a6351
ferrufinoda2 <ferrufinoda2@vcu.edu>
2024-05-03 11:38:04 -0700

    - attempted to get navigation working using jetpack compose. still not fully working has some bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.play.services.location)
     implementation(libs.androidx.media3.common)
     implementation(libs.osmbonuspack)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/uitutorial/MainActivity.kt
+++ app/src/main/java/com/example/uitutorial/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.uitutorial
 import GPSHandler
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Intent
+import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -16,17 +16,22 @@ import android.preference.PreferenceManager
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomAppBarDefaults
@@ -40,6 +45,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -53,10 +59,15 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.app.ActivityCompat
-import com.example.uitutorial.gpsDataHandler.PastTracksViewer
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.uitutorial.ui.theme.UITutorialTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.osmdroid.bonuspack.kml.KmlDocument
 import org.osmdroid.config.Configuration.getInstance
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
@@ -66,6 +77,7 @@ import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
 import org.osmdroid.views.overlay.gridlines.LatLonGridlineOverlay2
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+import java.io.File
 
 
 class MainActivity : ComponentActivity() {
@@ -121,8 +133,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         // Set content with Jetpack Compose
         setContent {
-            CheckLocationPermission {
-                BottomAppBarExample(map = this.map)
+            UITutorialTheme {
+                // Initialize the NavController
+                val navController = rememberNavController()
+
+                // Define the navigation graph
+                NavHost(navController, startDestination = "main") {
+                    composable("main") {
+                        // Your main screen content
+                        BottomAppBarExample(navController, map)
+                    }
+                    composable("pastTracksViewer") {
+                        // Content for the Past Tracks Viewer screen
+                        PastTracksViewerActivity(map)
+                    }
+                }
             }
         }
 
@@ -136,6 +161,15 @@ class MainActivity : ComponentActivity() {
     fun goToCurrentLocation(){
 
 
+    }
+
+    fun getFileList(): List<File> {
+        val tracksDir = File(this.filesDir, "tracks")
+        if (!tracksDir.exists()) {
+            tracksDir.mkdirs()
+        }
+        Log.d("PastTracksViewer", "files dir is " + tracksDir.canonicalPath)
+        return tracksDir.listFiles()?.toList() ?: emptyList()
     }
 
 
@@ -161,9 +195,7 @@ class MainActivity : ComponentActivity() {
         if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission granted, continue with your UI setup
-                setContent {
-                    BottomAppBarExample(map = MapView(this))
-                }
+
             } else {
                 // Permission denied, handle accordingly (e.g., show an explanation or disable location features)
                 // For simplicity, we'll just finish the activity
@@ -194,7 +226,98 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PastTracksViewerActivity(map: MapView) {
 
+    val context = LocalContext.current
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Top app bar")
+                }
+            )
+        },
+
+        bottomBar = {
+            BottomAppBar(
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(20.dp)),
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(Icons.Filled.Home, contentDescription = "Localized description")
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Edit,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Check,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Edit,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                },
+
+                )
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .clip(shape = RoundedCornerShape(20.dp))
+        ) {
+            // Display a list of clickable items using LazyColumn
+            LazyColumn()
+            {
+                val fileList = getFileList(context = context)
+                items(fileList) { file ->
+                    // Each item is contained within its own clickable block
+                    ClickableItem(file = file) {
+                        Log.d("ClickableItem", "You clicked on file: ${file.name}")
+                        var kmlDocument = KmlDocument()
+                        kmlDocument.parseKMLFile(file)
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ClickableItem(file: File, onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+    ) {
+        Text(text = file.name)
+    }
+}
+
+fun getFileList(context: Context): List<File> {
+        val tracksDir = File(context.filesDir, "tracks")
+        if (!tracksDir.exists()) {
+            tracksDir.mkdirs()
+        }
+        Log.d("PastTracksViewer", "files dir is " + tracksDir.canonicalPath)
+        return tracksDir.listFiles()?.toList() ?: emptyList()
+}
 
 
 @Composable
@@ -339,7 +462,7 @@ fun onMapChanged(mapView: MapView) {
 @SuppressLint("MissingPermission")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BottomAppBarExample(map: MapView) {
+fun BottomAppBarExample(navController: NavHostController, map1: MapView) {
 
     val context = LocalContext.current
     Scaffold(
@@ -361,10 +484,10 @@ fun BottomAppBarExample(map: MapView) {
                     .clip(shape = RoundedCornerShape(20.dp)),
                 actions = {
                     IconButton(onClick = {
-                        val intent = Intent(context, PastTracksViewer::class.java)
 
-                        context.startActivity(intent) }) {
-                        Icon(Icons.Filled.List, contentDescription = "Localized description")
+                        navController.navigate("pastTracksViewer")
+                        }) {
+                        Icon(Icons.Filled.List, contentDescription = "view past tracks")
                     }
                     IconButton(onClick = { /* do something */ }) {
                         Icon(
@@ -389,17 +512,17 @@ fun BottomAppBarExample(map: MapView) {
                     FloatingActionButton(
                         onClick = {
                             Log.d("Floating red Action Button" , "Button was pressed")
-                            for(overlay in map.overlays){
+                            for(overlay in map1.overlays){
 
                                 if(!overlay.toString().contains("MyLocation")){
-                                    map.overlays.remove(overlay)
+                                    map1.overlays.remove(overlay)
                                 }
                             }
 
                             val locationHandler = GPSHandler(context)
                             CoroutineScope(Dispatchers.IO).launch {
 
-                                TrackWriter().GPSTrackWriter(map)
+                                TrackWriter().GPSTrackWriter(map1)
                             }
 
                         },
@@ -426,7 +549,7 @@ fun BottomAppBarExample(map: MapView) {
             ) {
 
 
-            CustomView(map)
+            CustomView(map1)
 
         }
 
